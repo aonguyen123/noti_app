2c2d7362619c5d08a8063fca7a0e630ea869cd19
Đức Ảo <aonguyen113@gmail.com>
2020-09-30 16:15:48 +0700

    :lipstick: form login
    form login

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,15 +1,20 @@
 import React from "react";
-import { Form, Input, Grid, Card } from "@alifd/next";
+import { Form, Input, Grid, Card, Loading, Button } from "@alifd/next";
 
 const FormItem = Form.Item;
 const { Row, Col } = Grid;
 
+function ButtonSignIn() {
+  return <Button>SignIn</Button>;
+}
+
 export default function Login() {
   return (
-    <Row style={{height: '100vh'}} justify='center' wrap align='center'>
+    <Row style={{ height: "100vh" }} justify="center" wrap align="center">
       <Col xl={8} l={8} m={8} s={24} xs={24}>
         <Card free>
           <Card.Header title="Sign In" />
+
           <Card.Content>
             <Form labelTextAlign="left" size="medium" labelAlign="inset">
               <FormItem
@@ -28,18 +33,17 @@ export default function Login() {
                 hasFeedback
                 requiredMessage="Please input your password"
               >
-                <Input name="password" trim />
+                <Input name="password" htmlType="password" trim />
               </FormItem>
 
               <FormItem>
                 <Form.Submit
+                  component={ButtonSignIn}
                   style={{ width: "100%" }}
                   type="primary"
                   validate
                   //   onClick={this.handleSubmit}
-                >
-                  Submit
-                </Form.Submit>
+                />
               </FormItem>
             </Form>
           </Card.Content>
