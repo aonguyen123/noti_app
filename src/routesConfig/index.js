e84cbd798a8076de787c0509719c7fbae563487d
Đức Ảo <aonguyen113@gmail.com>
2020-10-02 14:11:14 +0700

    :triangular_flag_on_post: add nest router
    add nest router

--- src/routesConfig/index.js
+++ src/routesConfig/index.js
@@ -17,6 +17,13 @@ function Nesting() {
       <Route exact path={`${path}/test`}>
         <h1>Test1</h1>
       </Route>
+      <Route exact path={`${path}/not-found`}>
+        <h1>not found</h1>
+      </Route>
+      <Route path="/not-found">
+        <h1>Not Found1</h1>
+      </Route>
+      <Redirect to="/not-found" />
     </Switch>
   );
 }
@@ -44,6 +51,9 @@ function Config({ history, app }) {
           <Nesting />
         </Route>
         <Route exact path="/login" component={LoginPage} />
+        <Route exact path="/not-found">
+          <h1>Not Found</h1>
+        </Route>
       </Switch>
     </Router>
   );
